e3a6a430e48f6529c7b8b44f8b6c78325e64e48a
mdkhalid786698-create <mdkhalid786698@gmail.com>
2026-06-10 23:51:46 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 import streamlit as st
 
+st.set_page_config(
+    page_title="Kawaid Coin",
+    page_icon="🚀",
+    layout="wide"
+)
+
 # =========================
 # NAVBAR
 # =========================
